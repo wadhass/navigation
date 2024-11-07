b42ebf3a1c83270444c5c7ca9c40324bb88418b8
Ahmed <ahmedhassansumu@gmail.com>
2024-11-06 23:54:58 -0800

    worked on navigation

---
+++ index.js
@@ -0,0 +1,13 @@
+const navElement = document.getElementById('nav')
+
+const bodyElement = document.getElementById('body')
+
+const storeNavElement = localStorage.setItem
+
+
+if(window.location.pathname ==='about.html' || window.location.pathname === 'contact.html' ||)
+const copyElement = 
+
+bodyElement.innerHTML = navElement
+
+
